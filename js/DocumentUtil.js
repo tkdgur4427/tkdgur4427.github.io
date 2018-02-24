3f7d3133721f7bc34a77feb6e5fe3dd9a7966535
Brian Hong <tkdgur4427@outlook.com>
2018-02-25 00:40:06 +0900

    commit

---
+++ js/DocumentUtil.js
@@ -0,0 +1,9 @@
+﻿function embedPlayer(node, videoId) {
+    var iframe = document.createElement("iframe");
+    var url = window.location.protocol + "//www.youtube.com/embed/" + videoId + "?autoplay=1&autohide=1&rel=0&controls=2";
+    iframe.setAttribute("src", url);
+    iframe.setAttribute("frameborder", '0');
+    iframe.setAttribute("allowfullscreen", '');
+    iframe.className = "yt-embed";
+    node.parentNode.replaceChild(iframe, node);
+}
